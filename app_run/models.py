c7704e45cecd8174ea7a630927a1f075eb1ab66b
Ruslan Yarmak <ruslan.yarmak.intech@gmail.com>
2025-06-11 21:07:23 +0300

    bugfix

--- app_run/models.py
+++ app_run/models.py
@@ -31,4 +31,3 @@ class Position(models.Model):
     run = models.ForeignKey(Run, on_delete=models.CASCADE)
     latitude = models.DecimalField(max_digits=9, decimal_places=4, validators=[MinValueValidator(-90.0), MaxValueValidator(90.0)])
     longitude = models.DecimalField(max_digits=9, decimal_places=4, validators=[MinValueValidator(-180.0), MaxValueValidator(180.0)])
-    timestamp = models.DateTimeField(auto_now_add=True)
